c567c6e5d096b786c8be271fad8a4d8b314fbd2d
raulGLD <raulgldn@gmail.com>
2020-12-30 23:09:23 +0200

    Added some comments

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(client))  # prints message to console to verify the successful log in
 
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == client.user:  # check whether the bot sent the message or not, if it's the bot, do nothing
         return
 
-    if message.content.startswith('$hello'):
+    if message.content.startswith('$hello'):  # check for messages that start with "$hello" and respond
         await message.channel.send('Hello!')
 
 
-client.run('YOUR_BOT_TOKEN_HERE')
+client.run('YOUR_BOT_TOKEN_HERE')  # insert your bot token here, generated from Discord Developer Portal
